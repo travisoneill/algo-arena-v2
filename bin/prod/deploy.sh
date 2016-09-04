d369f6d591864e5bc4eb9c63efe670fc2c1689ca
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-09-04 15:40:06 -0700

    enable deploy script

--- bin/prod/deploy.sh
+++ bin/prod/deploy.sh
@@ -4,50 +4,47 @@ valid=false
 
 if [[ $1 == 'gateway' || $1 == 'all' ]]; then
   valid=true
-  echo gateway
-  # echo START DEPLOY GATEWAY
-  # cd gateway
-  # echo SETTING UP ENVIRONMENT
-  # sudo pip install virtualenv
-  # virtualenv -p python3 env
-  # source evn/bin/activate
-  # echo INSTALLING dependencies
-  # pip install -r requirements.txt
-  # echo DEPLOYING SERVICE
-  # gcloud app deploy
-  # echo FINISH DEPLOY GATEWAY
-  # deactivate
-  # cd ..
+  echo START DEPLOY GATEWAY
+  cd gateway
+  echo SETTING UP ENVIRONMENT
+  sudo pip install virtualenv
+  virtualenv -p python3 env
+  source evn/bin/activate
+  echo INSTALLING dependencies
+  pip install -r requirements.txt
+  echo DEPLOYING SERVICE
+  gcloud app deploy
+  echo FINISH DEPLOY GATEWAY
+  deactivate
+  cd ..
 fi
 
 if [[ $1 == 'flask' || $1 == 'all' ]]; then
   valid=true
-  echo flask
-  # echo START DEPLOY FLASK
-  # cd flask
-  # echo SETTING UP ENVIRONMENT
-  # sudo pip install virtualenv
-  # virtualenv -p python3 env
-  # source evn/bin/activate
-  # echo INSTALLING dependencies
-  # pip install -r requirements.txt
-  # echo DEPLOYING SERVICE
-  # gcloud app deploy
-  # echo FINISH DEPLOY FLASK
-  # deactivate
-  # cd ..
+  echo START DEPLOY FLASK
+  cd flask
+  echo SETTING UP ENVIRONMENT
+  sudo pip install virtualenv
+  virtualenv -p python3 env
+  source evn/bin/activate
+  echo INSTALLING dependencies
+  pip install -r requirements.txt
+  echo DEPLOYING SERVICE
+  gcloud app deploy
+  echo FINISH DEPLOY FLASK
+  deactivate
+  cd ..
 fi
 
 if [[ $1 == 'node' || $1 == 'all' ]]; then
   valid=true
-  echo node
-  #echo START DEPLOY NODE
-  # cd node
-  # echo SETTING UP ENVIRONMENT
-  # npm install
-  # gcloud app deploy
-  # echo FINISH DEPLOY NODE
-  # cd ..
+  echo START DEPLOY NODE
+  cd node
+  echo SETTING UP ENVIRONMENT
+  npm install
+  gcloud app deploy
+  echo FINISH DEPLOY NODE
+  cd ..
 fi
 
 if [ $valid == false ]; then
