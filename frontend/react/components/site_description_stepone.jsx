9e95c2e0f922d1947f0563a517ceb9dde829e65c
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-09-05 13:38:20 -0700

    change intro modal text to add python

--- frontend/react/components/site_description_stepone.jsx
+++ frontend/react/components/site_description_stepone.jsx
@@ -7,7 +7,7 @@ const SiteDescriptionOne = React.createClass({
       <div className='modal-container one'>
        <h2 className='welcome-text'>Welcome to Algo Arena</h2>
 
-       <p className='description-text'>Here at Algorithm Arena, we only care about two things. How fast your array sorting functions are, and how good you are with plain old JavaScript!</p>
+       <p className='description-text'>Make your sort alogrithms battle to the death.  Now in JavaScript and Python.</p>
        <img className='stepone-img' style={{'margin': '0 auto'}} src='https://storage.googleapis.com/algorithm-arena-static/intro.gif'></img>
 
      </div>
