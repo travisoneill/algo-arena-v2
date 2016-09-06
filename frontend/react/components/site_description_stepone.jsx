78cf8c7184bec9ca0920cf15b318df7a6a454572
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-09-05 18:07:33 -0700

    optimize bundle.js

--- frontend/react/components/site_description_stepone.jsx
+++ frontend/react/components/site_description_stepone.jsx
@@ -6,7 +6,7 @@ const SiteDescriptionOne = React.createClass({
       <div className='modal-container one'>
        <h2 className='welcome-text'>Welcome to Algo Arena</h2>
 
-       <p className='description-text'>Make your sort alogrithms battle to the death.  Now in JavaScript and Python.</p>
+       <p className='description-text'>Make your sort alogrithms battle to the death.  Now in JavaScript and Python. TEST NEW BUNDLE JS</p>
        <img className='stepone-img' style={{'margin': '0 auto'}} src='https://storage.googleapis.com/algorithm-arena-static/intro.gif'></img>
 
      </div>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,25 +1,71 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv === 'production';
 
 module.exports = {
-  context: __dirname,
-  entry: "./main.jsx",
+  devtool: isProd ? 'hidden-source-map' : 'cheap-eval-source-map',
+  context: path.join(__dirname, './'),
+  entry: {
+    js: './main.jsx',
+  },
   output: {
-    path: "./assets/",
-    filename: "bundle.js"
+    path: path.join(__dirname, './static/assets/'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /node_modules/,
-        loader: 'babel',
+        test: /\.html$/,
+        loader: 'file',
         query: {
-          presets: ['react', 'es2015']
+          name: '[name].[ext]'
         }
-      }
-    ]
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css'
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          // 'babel-preset-react',
+          'babel-loader',
+        ],
+        query: {
+          presets: ['react', 'es2015-native-modules']
+        }
+      },
+    ],
   },
   resolve: {
-    extensions: ["", ".js", ".jsx"]
+    extensions: ['', '.js', '.jsx'],
   },
-  devtool: 'source-map',
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+    })
+  ],
+  devServer: {
+    contentBase: './client'
+    // hot: true
+  }
 };
