6228528570fa45b8cf7aca576d510941f669f7b6
AviWaiz <avi.waizman@gmail.com>
2016-10-07 15:15:54 -0700

    start for a promise to handle responses

--- gateway_hapi/handle_req.js
+++ gateway_hapi/handle_req.js
@@ -1,3 +1,16 @@
-export const prom = new Promise((resolve, reject) => {
-  resolve()
-});
+const checkData = (res1, res2) => {
+    let results = [];
+    let promise = new Promise((resolve, reject) => {
+    if([res1, res2].every((res) => typeof res !== 'undefined')){
+      resolve([res1, res2]);
+    } else {
+      results.push(res1);
+      results.push(res2);
+    }
+  });
+  promise.then(data => sendToFrontend(data));
+};
+const sendToFrontend = (data) => {
+  // some callback that will take the data
+  //  from the and send it to the front
+};
