419920d4cd60e8d4c8634d0e7f09b3dd7a254148
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-10-08 21:18:33 -0700

    remove deprecated

--- frontend/main.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from './main'
-
-document.addEventListener('DOMContentLoaded', () =>{
-  const content = document.getElementById('content');
-  ReactDOM.render(<App />, content);
-});
